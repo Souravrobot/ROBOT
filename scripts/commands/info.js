bce6860ab13e3d4c6052e919e3a5b62e434e25ba
Souravrobot <167197751+Souravrobot@users.noreply.github.com>
2024-04-17 09:39:26 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -40,7 +40,7 @@ var callback = () => api.sendMessage({body:`
 𝐆𝐦𝐚𝐢𝐥       : sourabnath@gmail.com
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801880962478
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/𝚂𝙾𝚄𝚁𝙰𝚅 𝙽𝙰𝚃𝙷
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :https://www.facebook.com/profile.php?id=100075964867229`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/profile.php?id=100075964867229&mibextid=ZbWKwL`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://graph.facebook.com/https:/100075964867229/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
